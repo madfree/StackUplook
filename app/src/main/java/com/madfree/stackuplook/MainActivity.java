5ad1118863e9a7104e6806f439cccd3c26ee6677
mfrei <matzefrei@gmail.com>
2019-02-02 17:36:03 +0100

    Initial commit: Implemented Retrofit client and Stackoverflow Api

---
+++ app/src/main/java/com/madfree/stackuplook/MainActivity.java
@@ -0,0 +1,39 @@
+package com.madfree.stackuplook;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Call<StackApiResponse> call =
+                RetrofitClient.getsInstance()
+                .getApi()
+                .getAnswers(1, 50, "stackoverflow");
+
+        call.enqueue(new Callback<StackApiResponse>() {
+            @Override
+            public void onResponse(Call<StackApiResponse> call, Response<StackApiResponse>
+                    response) {
+                StackApiResponse stackApiResponse = response.body();
+
+                Toast.makeText(MainActivity.this, String.valueOf(stackApiResponse.items.size()), Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void onFailure(Call<StackApiResponse> call, Throwable t) {
+
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/madfree/stackuplook/RetrofitClient.java
@@ -0,0 +1,30 @@
+package com.madfree.stackuplook;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitClient {
+
+    private static final String BASE_URL = "https://api.stackexchange.com/2.2/";
+    private static RetrofitClient sInstance;
+    private Retrofit retrofit;
+
+    private RetrofitClient() {
+        retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+    }
+
+    public static synchronized RetrofitClient getsInstance() {
+        if (sInstance == null) {
+            sInstance = new RetrofitClient();
+        }
+        return sInstance;
+    }
+
+    public Api getApi() {
+        return retrofit.create(Api.class);
+    }
+
+}
